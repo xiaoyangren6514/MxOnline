073317887efbdfe21df61782ea15174b2c32aa09
zhonglongquan <zhonglongquan@rong360.com>
2017-06-29 11:40:36 +0800

    开始学习增加未登录校验

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 
 from courses.views import CouseListView, CourseDetailView, \
-    DemoView, AddFavView, CourseVideoView, CourseCommentView,CourseAddCommentView
+    DemoView, AddFavView, CourseInfoView, CourseCommentView,CourseAddCommentView
 
 urlpatterns = [
     url(r'^list/$', CouseListView.as_view(), name='course_list'),
     url(r'^detail/(?P<course_id>\d+)$', CourseDetailView.as_view(), name='course_detail'),
     url(r'^add_fav/$', AddFavView.as_view(), name='course_add_fav'),
-    url(r'^course_video/(?P<course_id>\d+)$', CourseVideoView.as_view(), name='course_video'),
+    url(r'^course_video/(?P<course_id>\d+)$', CourseInfoView.as_view(), name='course_video'),
     url(r'^course_comment/(?P<course_id>\d+)$', CourseCommentView.as_view(), name='course_comment'),
     url(r'^add_comment/$', CourseAddCommentView.as_view(), name='course_add_comment'),
     url(r'^demo/$', DemoView.as_view(), name='course_demo'),
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -6,7 +6,9 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Course
 
-from operation.models import UserFavorite, CourseComment
+from operation.models import UserFavorite, CourseComment, UserCourse
+
+from utils.mixin_utils import LoginRequiredMixin
 
 
 # Create your views here.
@@ -114,21 +116,45 @@ class AddFavView(View):
             return HttpResponse('{"status":"fail","msg":"用户未登录"}', content_type='application/json')
 
 
-class CourseVideoView(View):
+class CourseInfoView(LoginRequiredMixin, View):
+    """
+    课程章节信息
+    """
+
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
         all_lessons = course.lesson_set.all()
         all_resources = course.courseresource_set.all()
         teacher = course.teacher
+        # 取出学习过这门课程的所有数据
+        user_courses = UserCourse.objects.filter(course=course)
+        # 得到学习过这门课程的所有uid
+        user_ids = [user_course.user.id for user_course in user_courses]
+        # 得到相关用户学习过的所有课程
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        course_ids = [user_course.course.id for user_course in all_user_courses]
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
+        # 记录用户学习过当前课程
+        current_user_course_records = UserCourse.objects.filter(user_id=request.user.id, course_id=int(course.id))
+        if not current_user_course_records:
+            user_course_record = UserCourse()
+            user_course_record.user = request.user
+            user_course_record.course = course
+            user_course_record.save()
         return render(request, 'course-video.html', {
             'course': course,
             'all_lessons': all_lessons,
             'all_resources': all_resources,
-            'teacher': teacher
+            'teacher': teacher,
+            'all_user_courses': relate_courses
         })
 
 
 class CourseCommentView(View):
+    """
+    课程评论信息
+    """
+
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
         all_course_comments = course.coursecomment_set.all()
@@ -141,6 +167,10 @@ class CourseCommentView(View):
 
 
 class CourseAddCommentView(View):
+    """
+    添加评论
+    """
+
     def post(self, request):
         if not request.user.is_authenticated():
             return HttpResponse('{"status":"fail","msg":"用户未登录"}', content_type='application/json')
